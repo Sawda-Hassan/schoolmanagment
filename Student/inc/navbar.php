f4fc7d646d862f6ef8a5ea106553c1758eafa48b
Sawda-Hassan <milgomaariin@gmail.com>
2025-01-11 14:06:21 +0845

    scool managemrnt system

---
+++ Student/inc/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #162381;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="logo1.png" width="40" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navLinks">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php" style="color: white;">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="grade.php" style="color: yellow;">Grade Summary</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pass.php" style="color: white;">Change Password</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="../logout.php" style="color: white;">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ admin/inc/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #162381;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="./logo1.png" width="20" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navLinks">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php" style="color: #162381;">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="teacher.php" style="color: white">Teachers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="student.php" style="color:white;">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="grade.php" style="color: white">Grade</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="class.php" style="color: white">Class</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registrar-office.php" style="color: white">Registrar Office</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="course.php" style="color:white">Course</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="settings.php" style="color:white">Settings</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="../logout.php" style="color: #162381;">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
